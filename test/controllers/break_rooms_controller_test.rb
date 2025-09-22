4a8312256542d37f1c409dd7cd882d387ded1fac
koya <koyamaguchi.4@gmail.com>
2025-09-22 09:50:10 +0900

    テストコード修正

--- test/controllers/break_rooms_controller_test.rb
+++ test/controllers/break_rooms_controller_test.rb
@@ -1,18 +1,23 @@
 require "test_helper"
 
 class BreakRoomsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get break_rooms_index_url
-    assert_response :success
+  setup do
+    @project = projects(:one)      # test/fixtures/projects.yml から1件取得
+    @break_room = break_rooms(:one) # test/fixtures/break_rooms.yml がある前提
   end
 
-  test "should get edit" do
-    get break_rooms_edit_url
+  test "should get index" do
+    get project_break_rooms_url(@project)
     assert_response :success
   end
 
   test "should get new" do
-    get break_rooms_new_url
+    get new_project_break_room_url(@project)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_project_break_room_url(@project, @break_room)
     assert_response :success
   end
 end
